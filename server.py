5ca723183507fae923653d4be35ef3917b3b81a8
lightcoxa <42603470+lightcoxa@users.noreply.github.com>
2022-05-19 22:32:40 +0800

    update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import os 
+import os, hashlib
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
@@ -24,13 +24,42 @@ def upload_file():
                 dirSize = sum(os.path.getsize(f) for f in os.listdir('.') if os.path.isfile(f))
                 if dirSize < 50 * 1000 * 1000 * 1000:
                     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    return render_template("link.html", content=filename)
+                    fileSha1 = hashlib.sha1(filename.encode('utf-8'))
+                    fileCode = fileSha1.hexdigest()[:8]
+                    return render_template("link.html", content=fileCode)
                 else:
                     return redirect(request.url)
         except:
             return redirect(request.url)
 
     return render_template("upload.html")
+
+@app.route('/receive/<file>', methods=["GET", "POST"])
+def receive_file(file=""):
+    if request.method == "POST":
+        try:
+            fileCode = request.form.get("fileCode")
+            if fileName := checkFile(fileCode) == "":
+                return redirect(request.url)
+            return render_template("download.html", content=fileName)
+        except:
+            return redirect(request.url)
+    if file != "":
+        if fileName := checkFile(file) == "":
+            return redirect(request.url)
+        return render_template("download.html", content=fileName)
+    else:
+        return render_template("receive.html")
+
+def checkFile(fileCode):
+    if len(fileCode) == 8:
+        files = os.listdir(CONTAINER)
+        for i in files:
+            fileSha1 = hashlib.sha1(i.encode('utf-8'))
+            if fileSha1.hexdigest()[:8] == fileCode:
+                return i        
+        return ""
+    return ""
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Starting Flask example app.
-cd /home/azureuser/fileserver
-source .venv/bin/activate
-cd .venv
-gunicorn -w 2 -b 0.0.0.0:443 app:app
